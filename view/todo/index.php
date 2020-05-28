3ba245c602cfa9b660acf605b2bd3bfe6a9e5a09
m.nao <m.nao@m.lan>
2020-05-28 22:48:32 +0900

    Dispaly todos

--- view/todo/index.php
+++ view/todo/index.php
@@ -1,9 +1,40 @@
-const DNS = 'mysql:host = c248d86fe0a1';dbname = common;charset=utf-8;
+<?php
+const DSN = 'mysql:host=c248d86fe0a1;dbname=PHP_PRACTICE;charset=utf8';
 const USERNAME = 'MITARASHI';
 const PASSWORD = '1qazxsw2';
 
-$pdo = new PDO(DNS, USERNAME, PASSWORD);
-$stmh = $pdo->query('SELECT * FROM common.todos');
+$pdo = new PDO(DSN, USERNAME, PASSWORD);
+$stmh = $pdo->query('SELECT * FROM todos');
 
 $todo_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
-var_dump($todo_list);
+// var_dump($todo_list);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DB接続</title>
+  </head>
+  <body>
+    <ul>
+      <table border=1>
+        <tr>
+          <th>user_id</th>
+          <th>title</th>
+          <th>content</th>
+        </tr>
+        <?php foreach($todo_list as $todo): ?>
+          <tr>
+            <td><?php echo $todo['user_id']; ?></td>
+            <td><?php echo $todo['title']; ?></td>
+            <td><?php echo $todo['content']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+
+    </ul>
+  </body>
+</html>
