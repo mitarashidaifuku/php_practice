34d585ab3d10237dc30a70a808529bf2cf55a9f1
m.nao <m.nao@m.lan>
2020-05-29 22:32:51 +0900

    add if

--- view/todo/index.php
+++ view/todo/index.php
@@ -12,29 +12,34 @@ $todo_list = $stmh->fetchAll(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DB接続</title>
-  </head>
-  <body>
-    <ul>
-      <table border=1>
-        <tr>
-          <th>user_id</th>
-          <th>title</th>
-          <th>content</th>
-        </tr>
-        <?php foreach($todo_list as $todo): ?>
-          <tr>
-            <td><?php echo $todo['user_id']; ?></td>
-            <td><?php echo $todo['title']; ?></td>
-            <td><?php echo $todo['content']; ?></td>
-          </tr>
-        <?php endforeach; ?>
-      </table>
-
-    </ul>
-  </body>
+</head>
+<body>
+    <div class="">
+        <?php if($todo_list): ?>
+        <ul>
+            <table border=1>
+                <tr>
+                    <th>user_id</th>
+                    <th>title</th>
+                    <th>content</th>
+                </tr>
+                <?php foreach($todo_list as $todo): ?>
+                    <tr>
+                        <td><?php echo $todo['user_id']; ?></td>
+                        <td><?php echo $todo['title']; ?></td>
+                        <td><?php echo $todo['content']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </ul>
+        <?php else: ?>
+        s<div class="">データなし</div>
+        <?php endif;?>
+    </div>
+</body>
 </html>
